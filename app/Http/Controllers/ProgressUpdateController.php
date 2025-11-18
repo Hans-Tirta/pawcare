2a5ee6533374db7a356ab771511ef896a10996f6
Hans-Tirta <hansdominict@gmail.com>
2025-11-18 15:41:00 +0700

    Add controllers for Animal, Donation, ProgressUpdate, RescueCase, and Rescuer resources with basic CRUD methods

---
+++ app/Http/Controllers/ProgressUpdateController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProgressUpdate;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProgressUpdateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProgressUpdate $progressUpdate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProgressUpdate $progressUpdate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProgressUpdate $progressUpdate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProgressUpdate $progressUpdate)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/RescueCaseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RescueCase;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RescueCaseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RescueCase $rescueCase)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RescueCase $rescueCase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RescueCase $rescueCase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RescueCase $rescueCase)
+    {
+        //
+    }
+}
